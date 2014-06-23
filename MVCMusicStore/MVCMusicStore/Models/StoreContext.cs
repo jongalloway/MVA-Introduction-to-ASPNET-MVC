23182421682f84648a3e1147bf6cba789c5c4bd8
GeekTrainer <chrhar@microsoft.com>
2014-06-23 11:08:50 -0700

    Again

---
+++ MVCMusicStore/MVCMusicStore/Models/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCMusicStore.Models
+{
+	public class StoreContext : DbContext
+	{
+		public DbSet<Review> Reviews { get; set; }
+		public DbSet<Album> Albums { get; set; }
+		public DbSet<Artist> Artists { get; set; }
+
+	}
+}
