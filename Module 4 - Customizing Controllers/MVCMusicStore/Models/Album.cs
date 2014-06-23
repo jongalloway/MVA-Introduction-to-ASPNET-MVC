35dfe9245eec0a4e17b74893da2ce8371bca178b
Jon Galloway <jongalloway@gmail.com>
2014-06-23 14:14:08 -0700

    Added code for Module 4

---
+++ Module 4 - Customizing Controllers/MVCMusicStore/Models/Album.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MVCMusicStore.Models
+{
+	public class Album
+	{
+		public int AlbumID { get; set; }
+
+		public string Title { get; set; }
+
+		public Artist Artist { get; set; }
+
+		public virtual List<Review> Review { get; set; }
+	}
+}
