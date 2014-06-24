e44b182ce65e7cd7f3b88dd3d5964a95590494c8
GeekTrainer <chrhar@microsoft.com>
2014-06-23 17:06:30 -0700

    One last time

---
+++ Module 5 - Customizing Views/MVCMusicStore/MVCMusicStore/Models/Artist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCMusicStore.Models
+{
+	public class Artist
+	{
+		public int ArtistID { get; set; }
+
+		public string Name { get; set; }
+
+		public virtual List<Album> Albums { get; set; }
+
+	}
+}
